653a74a496a602812ca68016b179d587bcc1077c
Dov <dovpechman@gmail.com>
2020-11-14 18:01:57 -0500

    Added reason to the ResponseStatus annotation

--- src/main/java/com/acme/statusmgr/HandleBadRequest.java
+++ src/main/java/com/acme/statusmgr/HandleBadRequest.java
@@ -3,7 +3,7 @@ package com.acme.statusmgr;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Invalid details option: junkERROR")
 public class HandleBadRequest extends RuntimeException{
 
 	public HandleBadRequest(String message){
